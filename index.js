0bb34161803c9cd64cf2169c8f53f30d0f9ce16c
fardin-hasan <19-40043-1@student.aiub.edu>
2022-01-02 18:58:52 +0600

    first

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+require('dotenv').config()
+var cors = require('cors')
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+
+const app = express()
+app.use(cors());
+app.use(express.json())
+const port = process.env.PORT || 5000
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ex382.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ex382.mongodb.net/myFirstDatabase?retryWrites=true&w=majorit`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("realState");
+
+
+        //  api ###############################
+
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send('hey whats up!!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app ${port}`)
+})
